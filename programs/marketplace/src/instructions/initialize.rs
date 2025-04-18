fd9e4711c88960885930a61ffc133c6e58b17e54
stefano <stefano1990@gmail.com>
2025-04-18 17:48:48 +0200

    listing

--- programs/marketplace/src/instructions/initialize.rs
+++ programs/marketplace/src/instructions/initialize.rs
@@ -1,9 +1,57 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenInterface};
+
+use crate::state::marketplace::Marketplace;
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(name: String)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
 
-pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from: {:?}", ctx.program_id);
-    Ok(())
+    #[account(
+		init,
+		payer = admin,
+		seeds = [b"marketplace", name.as_str().as_bytes()],
+		bump,
+		space = Marketplace::INIT_SPACE,
+	)]
+    pub marketplace: Account<'info, Marketplace>,
+
+    // Doesn't need to be initialized, it's a system account
+    #[account(
+			seeds = [b"treasury", marketplace.key().as_ref()],
+			bump
+		)]
+    pub treasury: SystemAccount<'info>,
+
+    // This is going to be responsible for minting the rewards to the
+    // users account;
+    #[account(
+			init,
+			payer = admin,
+			seeds = [b"rewards", marketplace.key().as_ref()],
+			bump,
+			mint::decimals = 6,
+			mint::authority = marketplace
+		)]
+    pub reward_mint: InterfaceAccount<'info, Mint>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn init(&mut self, name: String, fee: u16, bumps: &InitializeBumps) -> Result<()> {
+        self.marketplace.set_inner(Marketplace {
+            admin: self.admin.key(),
+            fee,
+            bump: bumps.marketplace,
+            treasury_bump: bumps.treasury,
+            rewards_bump: bumps.reward_mint,
+            name,
+        });
+
+        Ok(())
+    }
 }
--- programs/marketplace/src/state/listing.rs
+++ programs/marketplace/src/state/listing.rs
@@ -3,11 +3,12 @@ use anchor_lang::prelude::*;
 #[account]
 pub struct Listing {
     pub maker: Pubkey,
+    // Nft that's going to be listed by the maker
     pub maker_mint: Pubkey,
     pub price: u64,
     pub bump: u8,
 }
 
 impl Space for Listing {
-    const INIT_SPACE: usize = 2 * 32 + 8 + 1;
+    const INIT_SPACE: usize = 8 + 2 * 32 + 8 + 1;
 }
